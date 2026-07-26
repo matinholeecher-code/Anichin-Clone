0ff84ca3de1028f783548affca5b4520bf27269c
matinholeecher-code <matinholeecher@gmail.com>
2026-07-27 05:50:47 +0800

    Update page.tsx

--- app/(public)/az-lists/page.tsx
+++ app/(public)/az-lists/page.tsx
@@ -1,10 +1,5 @@
-import { Suspense } from "react";
 import { ArchiveGrid } from "@/components/ArchiveGrid";
 
 export default function AzListsPage() {
-  return (
-    <Suspense fallback={<div className="p-6 text-center text-[#a0a0a0]">Memuat data...</div>}>
-      <ArchiveGrid title="AZ Lists" />
-    </Suspense>
-  );
+  return <ArchiveGrid title="AZ Lists" />;
 }
